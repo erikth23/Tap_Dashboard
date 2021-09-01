0f5c1e23fd8710908ff4bdc875af9b7f94d6f04d
Erik Thomas-Hommer <erikth2355@gmail.com>
2021-08-31 23:00:03 -0700

    Adding ability to see which rooms were cleaned that day

--- src/pages/Dashboard/Rooms.js
+++ src/pages/Dashboard/Rooms.js
@@ -3,25 +3,29 @@ import {Spinner} from 'reactstrap';
 import { DataStore } from 'aws-amplify';
 
 import {Asset} from '../../models'
+import {useCleaningTimes} from "../../helpers/hooks";
 
 const ROOM = 'ROOM'
 const statusToClass = new Map([
   [
-    'READY', 'btn-success'
+    'READY', 'success'
   ],
   [
-    'OCCUPIED', 'btn-dark'
+    'OCCUPIED', 'dark'
   ],
   [
-    'CLEAN', 'btn-warning'
+    'CLEAN', 'warning'
   ],
   [
-    'DIRTY', 'btn-danger'
+    'DIRTY', 'danger'
   ]
 ])
 
+const ONE_HALF_DAY = 43200000;
+
 const Rooms = ({systemID, roomChosen, setRoomChosen}) => {
 
+  const {times, isError, isLoading} = useCleaningTimes(systemID)
   const [assets, setAssets] = useState([]);
 
   useEffect(() => {
@@ -43,11 +47,20 @@ const Rooms = ({systemID, roomChosen, setRoomChosen}) => {
     }
   }
 
+  const isCleanedToday = (roomName) => {
+    return times && times.find(time => {
+      const start_time = new Date(time.startTime);
+
+      return time.name == roomName &&
+      Date.now() - start_time < ONE_HALF_DAY
+    })
+  }
+
 
   return (<React.Fragment>
     <div className="m-3">
       {
-        assets.sort((a, b) => parseInt(a.name) - parseInt(b.name)).map((room, key) => <button className={`btn-lg btn-room ${statusToClass.get(room.status)}`}
+        assets.sort((a, b) => parseInt(a.name) - parseInt(b.name)).map((room, key) => <button className={`btn-lg btn-room ${isCleanedToday(room.name) ? 'btn-' : 'btn-outline-'}${statusToClass.get(room.status)}`}
         onClick={() => setRoomChosen(room.name == roomChosen ? '' : room.name)}>
           {room.name}
         </button>)
